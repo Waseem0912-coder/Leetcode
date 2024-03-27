be8436bc0017ba0fc76d832f1ec21be3477d66e9
Waseem0912-coder <58933410+Waseem0912-coder@users.noreply.github.com>
2024-03-27 17:18:19 -0400

    Create bst_closest_target.py

---
+++ bst_closest_target.py
@@ -0,0 +1,11 @@
+#BST closedst to target
+
+def findClosestValueInBst(tree, target):
+    diff = INT_MAX
+    while 
+# This is the class of the input tree. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
