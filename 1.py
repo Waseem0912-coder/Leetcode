842e51468025aef9fe4f074ad598ed5474997e71
Waseem Ahmed <58933410+Waseem0912-coder@users.noreply.github.com>
2025-01-21 23:27:28 -0500

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        x=0
+        y=0
+        for a in nums:
+            y =0
+            for b in nums:
+                if y ==x:
+                    y = y+1
+                    continue
+                if y==0:
+                    y= y+1
+                    continue
+                if(a+b)==target:
+                    return [x,y]
+                else:
+                    y = y+1
+                    continue
+            x = x+1
+            continue
